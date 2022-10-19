9566914fa074744ebbcd2ab4bb8eba6410ff4541
Pablo Pérez <pablo@pabl.eu>
2022-10-19 13:47:46 +0200

    Added basic structure

--- pokemon.sh
+++ pokemon.sh
@@ -1,31 +1,57 @@
 #!/usr/bin/env bash
 
+function usage() {
+  echo "Bashémon: Proyecto SSOOI"
+  echo "Uso: $0 [-g]"
+  echo " -g: Mostrar los nombres de los integrantes del equipo"
+}
+
 function readCfg() {
+  :
 }
 
 function writeCfg() {
+  :
 }
 
 function mainMenu() {
+  :
 }
 
 function configMenu() {
+  :
 }
 
 function play() {
+  :
 }
 
 function statsMenu() {
+  :
 }
 
 function log() {
+  :
 }
 
 function randRange() {
+  :
 }
 
 function loadCoolPokegraphics() {
+  :
 }
 function coolGraphics() {
+  :
 }
 
+if [ $# -eq 0 ]; then
+  # programa
+  :
+elif [[ $# -eq 1 && "$1" == "-g" ]]; then
+  # nuestros nombres
+  :
+else
+  echo "ERROR: Argumentos introducidos inválidos."
+  usage
+fi
