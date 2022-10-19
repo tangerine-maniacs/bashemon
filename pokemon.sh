1788a4a473ab9fcd06594f817f92264bbd1af077
Pablo Pérez <pablo@pabl.eu>
2022-10-19 13:13:01 +0200

    Add functions

--- pokemon.sh
+++ pokemon.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
 
+function readCfg() {
+}
 
+function writeCfg() {
+}
+
+function mainMenu() {
+}
+
+function configMenu() {
+}
+
+function play() {
+}
+
+function statsMenu() {
+}
+
+function log() {
+}
+
+function randRange() {
+}
+
+function loadCoolPokegraphics() {
+}
+function coolGraphics() {
+}
 
